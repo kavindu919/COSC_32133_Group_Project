3fac59e7e8621145f3c06ed36d53b978514443b4
kavindu919 <isharagayanath579@gmail.com>
2024-04-27 15:54:27 +0530

    create login function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,26 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma.js";
 
-export const register = async (req, send) => {
+//create user
+export const register = async (req, res) => {
   const { username, email, password } = req.body;
+  try {
+    //hash the password
+    const hasedPassword = await bcrypt.hash(password, 10);
 
-  //hash the password
-  const hasedPassword = await bcrypt.hash(password, 10);
-  console.log(hasedPassword);
+    //create new user
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hasedPassword,
+      },
+    });
+    console.log(newUser);
+    res.status(201).json({ message: "User created sussesfull.." });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Invalid" });
+  }
 };
-export const login = (req, send) => {};
-export const logout = (req, send) => {};
